46a90cde7cb279bdbe55bf6f1ce2e93e47fc0e05
xx <xx>
2025-04-09 21:16:08 +0800

    deploy

--- next.config.mjs
+++ next.config.mjs
@@ -4,8 +4,8 @@ output: 'export',  // 启用静态导出
 images: {
     unoptimized: true,  // 禁用图片优化，因为静态导出不支持
 },
-basePath: '/next-tools', // 添加这行，因为GitHub Pages项目站点需要这个路径
-assetPrefix: '/next-tools/', // 添加这行，确保静态资源使用正确的路径
+// basePath: '/next-tools', // 添加这行，因为GitHub Pages项目站点需要这个路径
+// assetPrefix: '/next-tools/', // 添加这行，确保静态资源使用正确的路径
 };
 
 export default nextConfig;
