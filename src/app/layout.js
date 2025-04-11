4ebe97166eb4fe6a787d6ca1b14cb62d2bb27c92
xx <xx>
2025-04-11 17:45:50 +0800

    ga4

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import NavBar from "@/components/NavBar"; // 将导航栏逻辑移到子组件
+import GoogleAnalytics from "@/components/GoogleAnalytics";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,6 +24,7 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <GoogleAnalytics GA_MEASUREMENT_ID={process.env.NEXT_PUBLIC_GA_ID}></GoogleAnalytics>
         <NavBar />
         <main className="min-h-screen bg-gray-50">{children}</main>
       </body>
